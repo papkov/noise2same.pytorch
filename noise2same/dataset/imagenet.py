664721f246c3aa85666de0c1d7939c90c6450a8a
papkov <mikhail.papkov@gmail.com>
2021-06-18 17:53:59 +0300

    Add imagenet

---
+++ noise2same/dataset/imagenet.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Union
+
+import numpy as np
+
+from noise2same.dataset.abc import AbstractNoiseDataset2D
+
+
+@dataclass
+class ImagenetDatasetPrepared(AbstractNoiseDataset2D):
+    path: Union[Path, str] = "data/ImageNet"
+    mode: str = "train"
+    version: int = 0  # two noisy copies exist (0, 1)
+
+    def _validate(self) -> bool:
+        assert self.mode in ("train", "val")
+        assert self.version in (0, 1)
+        return True
+
+    def _get_images(self) -> Union[List[str], np.ndarray]:
+        return np.load(self.path / f"{self.mode}.npy")[:, self.version + 1]
+
+    def _read_image(self, image_or_path: Union[str, np.ndarray]) -> np.ndarray:
+        return image_or_path
+
+
+@dataclass
+class ImagenetDatasetTest(AbstractNoiseDataset2D):
+    path: Union[Path, str] = "data/ImageNet"
+
+    def _get_images(self) -> Union[List[str], np.ndarray]:
+        return sorted((self.path / "test").glob("*.npy"))
+
+    def _read_image(self, image_or_path: Union[str, np.ndarray]) -> np.ndarray:
+        return np.load(image_or_path)[1]
