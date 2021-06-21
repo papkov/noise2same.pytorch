c105e1391702819bda2d780b10bde61984730ff7
papkov <mikhail.papkov@gmail.com>
2021-06-21 18:38:22 +0300

    Add mask genetarion tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 from albumentations import PadIfNeeded
 
+from noise2same.dataset.util import mask_like_image
 from noise2same.util import crop_as
 
 
@@ -22,6 +23,22 @@ class TestDataset(unittest.TestCase):
             print(padded.shape, cropped.shape)
             self.assertEqual(cropped.shape, image.shape)
             self.assertTrue(np.all(cropped == image))
+
+    def test_mask_2d(self, mask_percentage: float = 0.5):
+        shape = (64, 64, 3)
+        img = np.zeros(shape)
+        mask = mask_like_image(img, mask_percentage=mask_percentage, channels_last=True)
+        result = mask.mean() * 100
+        self.assertAlmostEqual(mask_percentage, result, delta=0.1)
+
+    def test_mask_3d(self, mask_percentage: float = 0.5):
+        shape = (1, 16, 64, 64)
+        img = np.zeros(shape)
+        mask = mask_like_image(
+            img, mask_percentage=mask_percentage, channels_last=False
+        )
+        result = mask.mean() * 100
+        self.assertAlmostEqual(mask_percentage, result, delta=0.1)
 
 
 if __name__ == "__main__":
