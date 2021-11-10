ca91eaa30506438fedb0cdac1f67da1576d1de70
papkov <mikhail.papkov@gmail.com>
2021-11-10 22:16:17 +0200

    Move evaluation from Trainer to Evaluator

---
+++ noise2same/evaluator.py
@@ -0,0 +1,212 @@
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
+import torch
+from pytorch_toolbelt.inference.tiles import TileMerger
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm, trange
+
+from noise2same.dataset.util import PadAndCropResizer
+from noise2same.model import Noise2Same
+from noise2same.util import load_checkpoint_to_module
+
+
+class Evaluator(object):
+    def __init__(
+        self,
+        model: Noise2Same,
+        device: str = "cuda",
+        checkpoint_path: Optional[str] = None,
+    ):
+        """
+        Model evaluator, describes inference for different data formats
+        :param model: model architecture to evaluate
+        :param device: str, device to run inference
+        :param checkpoint_path: optional str, path to the model checkpoint
+        """
+
+        self.model = model
+        self.device = device
+        self.checkpoint_path = checkpoint_path
+        if checkpoint_path is not None:
+            self.load_checkpoint(checkpoint_path)
+
+        self.model.to(device)
+
+        self.resizer = PadAndCropResizer(
+            mode="reflect", div_n=2 ** self.model.net.depth
+        )
+
+    @torch.no_grad()
+    def inference(
+        self, loader: DataLoader, half: bool = False, empty_cache: bool = False
+    ) -> List[Dict[str, np.ndarray]]:
+        """
+        Run inference for a given dataloader
+        :param loader: DataLoader
+        :param half: bool, if use half precision
+        :param empty_cache: bool, if empty CUDA cache after each iteration
+        :return: List[Dict[key, output]]
+        """
+        self.model.eval()
+        if half:
+            self.model.half()
+        else:
+            self.model.float()
+
+        outputs = []
+        iterator = tqdm(loader, desc="inference", position=0, leave=True)
+        for i, batch in enumerate(iterator):
+            if half:
+                batch = {k: v.half() for k, v in batch.items()}
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+            out = self.model(batch["image"])
+
+            out_raw = out["image"] * batch["std"] + batch["mean"]
+            out_raw = {"image": np.moveaxis(out_raw.detach().cpu().numpy(), 1, -1)}
+            if self.model.lambda_proj > 0:
+                out_raw.update(
+                    {"proj": np.moveaxis(out["proj"].detach().cpu().numpy(), 1, -1)}
+                )
+
+            outputs.append(out_raw)
+            iterator.set_postfix(
+                {
+                    "shape": out_raw["image"].shape,
+                    "reserved": torch.cuda.memory_reserved(0) / (1024 ** 2),
+                    "allocated": torch.cuda.memory_allocated(0) / (1024 ** 2),
+                }
+            )
+            if empty_cache:
+                torch.cuda.empty_cache()
+
+        return outputs
+
+    @torch.no_grad()
+    def inference_single_image_dataset(
+        self,
+        dataset: Dataset,
+        batch_size: int = 1,
+        num_workers: int = 0,
+        crop_border: int = 0,
+        device: str = "cpu",
+        half: bool = False,
+        empty_cache: bool = False,
+        key: str = "image",
+    ) -> np.ndarray:
+        """
+        Run inference for a single image represented as Dataset
+        Here, we assume that dataset was tiled and has a `tiler` attribute
+
+        :param dataset: Dataset representing a single large tiled image
+        :param batch_size: int, batch size for DataLoader
+        :param num_workers: int, number of workers for DataLoader
+        :param crop_border: int, border pixels to crop when merging tiles
+        :param device: str, device where to accumulate merging tiles
+        :param half: bool, if use half precision
+        :param empty_cache: bool, if empty CUDA cache after
+        :param key: str, which output key to accumulate
+        :return: numpy array, merged image
+        """
+        assert hasattr(dataset, "tiler"), "Dataset should have a `tiler` attribute"
+
+        self.model.eval()
+        if half:
+            self.model.half()
+        else:
+            self.model.float()
+
+        merger = TileMerger(
+            dataset.tiler.target_shape,
+            channels=self.model.in_channels,
+            weight=dataset.tiler.weight,
+            device=device,
+            crop_border=crop_border,
+            default_value=0,
+        )
+        # print(f'Created merger for image {merger.image.shape}')
+
+        iterator = dataset
+        if batch_size > 1:
+            iterator = torch.utils.data.DataLoader(
+                dataset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                shuffle=False,
+                pin_memory=False,
+                drop_last=False,
+            )
+        iterator = tqdm(iterator, desc="Predict")
+
+        for i, batch in enumerate(iterator):
+            # We can iterate over dataset, hence need to unsqueeze batch dim
+            if batch_size == 1:
+                batch["image"] = batch["image"][None, ...]
+                batch["crop"] = batch["crop"][None, ...]
+
+            # We don't need to half and move to device for `crop`
+            if half:
+                batch = {k: v.half() if k != "crop" else v for k, v in batch.items()}
+            batch = {
+                k: v.to(self.device) if k != "crop" else v for k, v in batch.items()
+            }
+
+            pred_batch = self.model(batch["image"])[key] * batch["std"] + batch["mean"]
+            iterator.set_postfix(
+                {
+                    "in_shape": tuple(batch["image"].shape),
+                    "out_shape": tuple(pred_batch.shape),
+                    "crop": batch["crop"],
+                }
+            )
+
+            merger.integrate_batch(batch=pred_batch, crop_coords=batch["crop"])
+            if empty_cache:
+                torch.cuda.empty_cache()
+
+        merger.merge_()
+        return dataset.tiler.crop_to_original_size(merger.image.cpu().numpy()[0])
+
+    @torch.no_grad()
+    def inference_single_image_tensor(
+        self,
+        image: torch.Tensor,
+        standardize: bool = True,
+        im_mean: Optional[float] = None,
+        im_std: Optional[float] = None,
+    ) -> torch.Tensor:
+        """
+        Run inference for a single image represented as Dataset
+
+        :param image: torch.Tensor
+        :param standardize: bool, if subtract mean and divide by std
+        :param im_mean: float, precalculated image mean
+        :param im_std: float, precalculated image std
+        :return: torch.Tensor
+        """
+
+        if not standardize:
+            im_mean, im_std = 0, 1
+
+        if im_mean is None:
+            im_mean = image.mean()
+
+        if im_std is None:
+            im_std = image.std()
+
+        image = (image - im_mean) / im_std
+
+        image = self.resizer.before(image, exclude=0)[None, ...]
+        out = self.model(image.to(self.device)).detach().cpu()
+        out = self.resizer.after(out[0], exclude=0)
+        out = out * im_std + im_mean
+
+        return out
+
+    def load_checkpoint(self, checkpoint_path: str):
+        checkpoint = torch.load(checkpoint_path, map_location="cpu")
+        if "model" in checkpoint:
+            checkpoint["model"].pop("mask_kernel.kernel", None)
+            checkpoint = checkpoint["model"]
+        self.model.load_state_dict(checkpoint, strict=False)
--- noise2same/trainer.py
+++ noise2same/trainer.py
@@ -1,18 +1,16 @@
 from collections import Counter
 from pathlib import Path
-from typing import Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
 import torch
 import wandb
-from pytorch_toolbelt.inference.tiles import TileMerger
-from torch import Tensor
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from tqdm import tqdm, trange
 
-from noise2same.dataset.util import PadAndCropResizer
+from noise2same.evaluator import Evaluator
 from noise2same.model import Noise2Same
-from noise2same.util import crop_as
+from noise2same.util import load_checkpoint_to_module
 
 
 class Trainer(object):
@@ -41,9 +39,7 @@ class Trainer(object):
 
         self.model.to(device)
         self.checkpoint_path.mkdir(parents=True, exist_ok=False)
-        self.resizer = PadAndCropResizer(
-            mode="reflect", div_n=2 ** self.model.net.depth
-        )
+        self.evaluator = Evaluator(model=model, device=device)
 
     def one_epoch(
         self, loader: DataLoader
@@ -126,126 +122,14 @@ class Trainer(object):
 
         return {"val_rec_mse": total_loss / len(loader)}, images
 
-    @torch.no_grad()
-    def inference(
-        self, loader: DataLoader, half: bool = False, empty_cache: bool = False
-    ) -> List[np.ndarray]:
-        self.model.eval()
-        if half:
-            self.model.half()
+    def inference(self, **kwargs: Any):
+        return self.evaluator.inference(**kwargs)
 
-        outputs = []
-        iterator = tqdm(loader, desc="inference", position=0, leave=True)
-        for i, batch in enumerate(iterator):
-            if half:
-                batch = {k: v.half() for k, v in batch.items()}
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            out_raw = self.model(batch["image"])["image"] * batch["std"] + batch["mean"]
-            out_raw = np.moveaxis(out_raw.detach().cpu().numpy(), 1, -1)
-            outputs.append(out_raw)
-            iterator.set_postfix(
-                {
-                    "shape": out_raw.shape,
-                    "reserved": torch.cuda.memory_reserved(0) / (1024 ** 2),
-                    "allocated": torch.cuda.memory_allocated(0) / (1024 ** 2),
-                }
-            )
-            if empty_cache:
-                torch.cuda.empty_cache()
+    def inference_single_image_dataset(self, **kwargs: Any):
+        return self.evaluator.inference_single_image_dataset(**kwargs)
 
-        return outputs
-
-    @torch.no_grad()
-    def inference_large(
-        self,
-        test_ds: Dataset,
-        batch_size: int = 1,
-        crop_border: int = 0,
-        device: str = "cpu",
-        num_workers: int = 0,
-        half: bool = False,
-    ):
-        self.model.eval()
-        if half:
-            self.model.half()
-        else:
-            self.model.float()
-
-        merger = TileMerger(
-            test_ds.tiler.target_shape,
-            channels=self.model.in_channels,
-            weight=test_ds.tiler.weight,
-            device=device,
-            crop_border=crop_border,
-            default_value=0,
-        )
-        # print(f'Created merger for image {merger.image.shape}')
-
-        iterator = test_ds
-        if batch_size > 1:
-            iterator = torch.utils.data.DataLoader(
-                test_ds,
-                batch_size=batch_size,
-                num_workers=num_workers,
-                shuffle=False,
-                pin_memory=False,
-                drop_last=False,
-            )
-        iterator = tqdm(iterator, desc="Predict")
-
-        for i, batch in enumerate(iterator):
-            # Iterate over dataset, hence need to unsqueeze batch dim
-            if batch_size == 1:
-                batch["image"] = batch["image"][None, ...]
-                batch["crop"] = batch["crop"][None, ...]
-
-            if half:
-                batch = {k: v.half() if k != "crop" else v for k, v in batch.items()}
-            batch = {
-                k: v.to(self.device) if k != "crop" else v for k, v in batch.items()
-            }
-
-            pred_batch = (
-                self.model(batch["image"])["image"] * batch["std"] + batch["mean"]
-            )
-            iterator.set_postfix(
-                {
-                    "in_shape": tuple(batch["image"].shape),
-                    "out_shape": tuple(pred_batch.shape),
-                    "crop": batch["crop"],
-                }
-            )
-
-            merger.integrate_batch(batch=pred_batch, crop_coords=batch["crop"])
-
-        merger.merge_()
-        return test_ds.tiler.crop_to_original_size(merger.image.cpu().numpy()[0])
-
-    @torch.no_grad()
-    def inference_image(
-        self,
-        image: Tensor,
-        standardize: bool = True,
-        im_mean: Optional[float] = None,
-        im_std: Optional[float] = None,
-    ):
-        if not standardize:
-            im_mean, im_std = 0, 1
-
-        if im_mean is None:
-            im_mean = image.mean()
-
-        if im_std is None:
-            im_std = image.std()
-
-        image = (image - im_mean) / im_std
-
-        image = self.resizer.before(image, exclude=0)[None, ...]
-        out = self.model(image.to(self.device)).detach().cpu()
-        out = self.resizer.after(out[0], exclude=0)
-        out = out * im_std + im_mean
-
-        return out
+    def inference_single_image_tensor(self, **kwargs: Any):
+        return self.evaluator.inference_single_image_tensor(**kwargs)
 
     def fit(
         self,
@@ -315,7 +199,7 @@ class Trainer(object):
         torch.save(
             {
                 "model": self.model.state_dict(),
-                "training": self.optimizer.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
                 "scheduler": self.scheduler.state_dict(),
             },
             f"{self.checkpoint_path}/{name}.pth",
@@ -324,11 +208,4 @@ class Trainer(object):
     def load_model(self, path: Optional[str] = None):
         if path is None:
             path = self.checkpoint_path
-        checkpoint = torch.load(path)
-        for attr, state_dict in checkpoint.items():
-            try:
-                getattr(self, attr).load_state_dict(state_dict)
-            except AttributeError:
-                print(
-                    f"Attribute {attr} is present in the checkpoint but absent in the class, do not load"
-                )
+        load_checkpoint_to_module(self, path)
