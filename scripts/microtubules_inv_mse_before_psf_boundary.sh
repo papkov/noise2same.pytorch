d65d5a10f35208f43aed92f3a750025cb9a03a39
papkov <mikhail.papkov@gmail.com>
2022-03-11 16:14:15 +0200

    Modify microtubules experiment

--- scripts/microtubules_inv_mse_before_psf_boundary.sh
+++ scripts/microtubules_inv_mse_before_psf_boundary.sh
@@ -1,1 +1,4 @@
-python train.py +experiment=microtubules device=1 model.inv_mse_key=deconv model.regularization_key=deconv project=noise2same-ssi-mt model.lambda_bound=0.1
+python train.py +experiment=microtubules_generated project=noise2same-ssi-mt-gen \
+       model.lambda_inv=0 model.lambda_inv_deconv=2 \
+       model.regularization_key=deconv model.lambda_bound=0.1 \
+       device=5
