4198f38797bfd89312f5ec564e94cd853a740701
pchizhov <chizhov@login2.hpc.ut.ee>
2022-12-17 13:26:04 +0200

    Rewrite WindowAttention

--- noise2same/backbone/swinir.py
+++ noise2same/backbone/swinir.py
@@ -5,6 +5,7 @@
 
 import torch
 import einops
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
@@ -69,35 +70,34 @@ class WindowAttention(nn.Module):
         proj_drop (float, optional): Dropout ratio of output. Default: 0.0
     """
 
-    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
-
+    def __init__(self, embed_dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
-        self.dim = dim
-        self.window_size = window_size  # Wh, Ww
+        self.embed_dim = embed_dim
+        self.window_size = window_size
         self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.scale = qk_scale or (embed_dim // num_heads) ** -0.5
+        self.num_pixels = np.prod(window_size).item()
 
-        # define a parameter table of relative position bias
-        self.relative_position_bias_table = nn.Parameter(
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+        window_bias_shape = [2 * s - 1 for s in window_size]
+        self.relative_position_bias_table = nn.Parameter(torch.zeros(np.prod(window_bias_shape).item(), num_heads))
 
-        # get pair-wise relative position index for each token inside the window
-        coords_h = torch.arange(self.window_size[0])
-        coords_w = torch.arange(self.window_size[1])
-        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
-        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
-        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
-        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
-        relative_coords[:, :, 0] += self.window_size[0] - 1  # shift to start from 0
-        relative_coords[:, :, 1] += self.window_size[1] - 1
-        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
-        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+        coords = torch.stack(torch.meshgrid([torch.arange(s) for s in window_size], indexing='ij'))
+        coords_flatten = torch.flatten(coords, 1)
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+        relative_coords = torch.einsum("n...->...n", relative_coords)
+
+        coefficients = [np.prod(window_bias_shape[i:]) for i in range(1, len(window_size))]
+        for dim, size in enumerate(window_size):
+            relative_coords[..., dim] += size - 1
+            if dim < len(window_size) - 1:
+                relative_coords[..., dim] *= coefficients[dim]
+
+        relative_position_index = relative_coords.sum(-1).flatten()
         self.register_buffer("relative_position_index", relative_position_index)
 
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.qkv = nn.Linear(embed_dim, embed_dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
+        self.proj = nn.Linear(embed_dim, embed_dim)
 
         self.proj_drop = nn.Dropout(proj_drop)
 
@@ -110,29 +110,25 @@ class WindowAttention(nn.Module):
             x: input features with shape of (num_windows*B, N, C)
             mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
         """
-        B_, N, C = x.shape
-        qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
+        q, k, v = einops.rearrange(self.qkv(x), "nw n (s nh ch) -> s nw nh n ch", s=3, nh=self.num_heads)
 
         q = q * self.scale
-        attn = (q @ k.transpose(-2, -1))
+        attn = torch.einsum("...ik,...jk->...ij", q, k)
 
-        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
-        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+        relative_position_bias = einops.rearrange(
+            self.relative_position_bias_table[self.relative_position_index],
+            "(np1 np2) nh -> nh np1 np2", np1=self.num_pixels
+        )
         attn = attn + relative_position_bias.unsqueeze(0)
 
         if mask is not None:
-            nW = mask.shape[0]
-            attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
-            attn = attn.view(-1, self.num_heads, N, N)
-            attn = self.softmax(attn)
-        else:
-            attn = self.softmax(attn)
+            attn = einops.rearrange(attn, "(b nw) ... -> b nw ...", nw=mask.shape[0]) + mask.unsqueeze(1).unsqueeze(0)
+            attn = einops.rearrange(attn, "b nw ... -> (b nw) ...", nw=mask.shape[0])
 
+        attn = self.softmax(attn)
         attn = self.attn_drop(attn)
 
-        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = einops.rearrange(attn @ v, "nw nh n ch -> nw n (nh ch)")
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
