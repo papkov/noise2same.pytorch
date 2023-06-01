218a6dcbd65524102b25413c1127a4100ba39054
papkov <mikhail.papkov@gmail.com>
2023-06-01 15:56:59 +0300

    Add autoencoder denoiser to init

--- noise2same/denoiser/__init__.py
+++ noise2same/denoiser/__init__.py
@@ -1,2 +1,3 @@
 from noise2same.denoiser.noise2self import Noise2Self, Noise2SelfDeconvolution
 from noise2same.denoiser.noise2same import Noise2Same, Noise2SameDeconvolution
+from noise2same.denoiser.abc import Denoiser
