e0d50aeecca83642968a5de9b60bcfdc19883011
pchizhov <chizhov.pasha@yandex.ru>
2023-06-19 11:23:26 +0300

    Test tiled inference on ImageNet and Planaria

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -13,6 +13,8 @@ from omegaconf import OmegaConf
 from noise2same.dataset import *
 from noise2same.dataset.getter import get_test_dataset_and_gt, expand_dataset_cfg
 from noise2same.dataset.util import mask_like_image, add_noise
+from noise2same.denoiser import Denoiser
+from noise2same.evaluator import Evaluator
 from noise2same.util import crop_as_gt
 
 
@@ -123,7 +125,7 @@ def test_dataset_instantiation(dataset_name: str, expected_dataclass: type, expe
                           ('microtubules_generated', MicrotubulesDataset),
                           ('fmd', FMDDataset),
                           ('fmd_deconvolution', FMDDataset),
-                          # ('planaria', PlanariaDataset), # TODO fix memory issue
+                          # ('planaria', PlanariaTestDataset), # TODO fix memory issue
                           # ('sidd', SIDDDataset), # TODO move dataset
                           ('synthetic', SyntheticTestDataset),
                           ('synthetic_grayscale', SyntheticTestDataset),
@@ -165,3 +167,23 @@ def test_concat_dataset(dataset_name):
         expand_dataset_cfg(cfg)
         print('\n', OmegaConf.to_yaml(cfg))
         _ = instantiate(cfg.dataset_test)
+
+
+@pytest.mark.parametrize('dataset_name', ['imagenet', 'planaria'])
+def test_tiled_dataset_inference(dataset_name: str):
+    with initialize(version_base=None, config_path="../config/experiment"):
+        overrides = ['+backbone_name=unet', '+backbone.depth=3', '+cwd=${hydra.runtime.cwd}']
+        if dataset_name == 'synthetic':
+            # Do not use cache for testing because of memory issues
+            overrides.append('dataset.cached=null')
+
+        cfg = compose(config_name=dataset_name, return_hydra_config=True, overrides=overrides)
+        OmegaConf.resolve(cfg)  # resolves interpolations as ${hydra.runtime.cwd}
+        expand_dataset_cfg(cfg)
+        print('\n', OmegaConf.to_yaml(cfg))
+
+    dataset = instantiate(cfg.dataset_test)
+    factory = instantiate(cfg.factory_test)
+    dataset.image_index = {k: v[:2] for k, v in dataset.image_index.items()}
+    evaluator = Evaluator(Denoiser(), device='cpu')
+    _ = evaluator.evaluate(dataset, factory)
