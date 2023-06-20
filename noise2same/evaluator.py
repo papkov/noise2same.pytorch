ad5431fedbf52ec9f64ae68e8fc168b828b7464d
pchizhov <chizhov.pasha@yandex.ru>
2023-06-20 18:39:59 +0300

    Transfer only the image to device for inference

--- noise2same/evaluator.py
+++ noise2same/evaluator.py
@@ -349,18 +349,18 @@ class Evaluator(object):
         empty_cache: bool = False,
         key: str = 'image',
     ) -> Tuple[Dict[str, T], float]:
-        batch = {k: v.to(self.device) if k in ['image', 'ground_truth', 'std', 'mean'] else v for k, v in batch.items()}
         start = time.time()
+        out = batch.copy()
         with autocast(enabled=half):
             try:
-                batch['image'] = self.model.forward(batch['image'])[key]
+                out['image'] = self.model.forward(batch['image'].to(self.device))[key]
             except RuntimeError as e:
-                raise RuntimeError(f"Error during inference on batch {batch['image'].shape}, "
+                raise RuntimeError(f"Error during inference on batch {out['image'].shape}, "
                                    f"half={half}, empty_cache={empty_cache}, key={key}") from e
         end = time.time()
         if empty_cache:
             torch.cuda.empty_cache()
-        return batch, end - start
+        return out, end - start
 
     def _revert_batch(
             self,
