969450fc87e64d2715172e13bf7fd8316ae6bf51
papkov <mikhail.papkov@gmail.com>
2023-06-24 16:57:09 +0300

    Add noise and blur for hela

--- noise2same/dataset/hela.py
+++ noise2same/dataset/hela.py
@@ -6,7 +6,16 @@ import h5py
 import numpy as np
 
 from noise2same.dataset.abc import AbstractNoiseDataset
+from noise2same.dataset.util import (
+    add_microscope_blur_3d,
+    add_noise,
+    normalize,
+)
 from noise2same.util import normalize_percentile
+import logging
+
+log = logging.getLogger(__name__)
+
 
 def read_h5py(path):
     with h5py.File(path, "r") as f:
@@ -16,19 +25,24 @@ def read_h5py(path):
 
 @dataclass
 class HelaShallowDataset(AbstractNoiseDataset):
-    path: Union[Path, str] = "data/hela"
+    path: Union[Path, str] = "data/hela/reconvolved"
     channel_id: int = 3
     mode: str = "train"
-    standardize: bool = False
+    add_blur_and_noise: bool = False
 
     def __str__(self) -> str:
         return f'hela_shallow_{self.mode}_ch{self.channel_id}'
 
     def _create_image_index(self) -> Dict[str, Union[List[str], np.ndarray]]:
-        input_files = sorted(list(Path(self.path).glob(f"reconvolved/*ch{self.channel_id}.h5")))
-        gt_files = sorted(list(Path(self.path).glob(f"deconvolved/*ch{self.channel_id}.h5")))
+        self.path = Path(self.path)
+        path_gt = self.path.parent / 'deconvolved'
+        pattern = f"*ch{self.channel_id}.h5"
+        input_files = sorted(list(self.path.glob(pattern)))
+        gt_files = sorted(list(path_gt.glob(pattern)))
+
         assert len(input_files) > 0, f"no files found in {self.path}"
         assert len(input_files) == len(gt_files), f"input and gt files have different lengths"
+
         if self.mode == "train":
             input_files = input_files[:-1]
             gt_files = gt_files[:-1]
@@ -38,8 +52,19 @@ class HelaShallowDataset(AbstractNoiseDataset):
         else:
             raise ValueError(f"unknown mode {self.mode}")
 
-        return {'image': np.stack([normalize_percentile(read_h5py(f), 0.1, 99.9) for f in input_files]),
-                'ground_truth': np.stack([normalize_percentile(read_h5py(f), 0.1, 99.9) for f in gt_files])}
+        image_index = {'image': [read_h5py(f) for f in input_files],
+                       'ground_truth': [normalize_percentile(read_h5py(f), 0.1, 99.9) for f in gt_files]}
+
+        if self.add_blur_and_noise:
+            for i, _ in enumerate(image_index['image']):
+                log.info(f"Generating blur and noise for image {i} in {self.mode} stack")
+                image_index['image'][i] = normalize(image_index['image'][i])
+                image_index['image'][i], self.psf = add_microscope_blur_3d(image_index['image'][i], size=17)
+                image_index['image'][i] = add_noise(image_index['image'][i],
+                                                    alpha=0.001, sigma=0.01, quant_bits=10)
+
+        image_index = {k: np.stack(v) for k, v in image_index.items()}
+        return image_index
 
     def get_number_of_images(self) -> int:
         return self.image_index['image'].shape[0] * self.image_index['image'].shape[1]
