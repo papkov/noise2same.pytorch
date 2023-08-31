ab44b56fe0fc8d9cd560fca7abfb7fec37bdf76f
pchizhov <chizhov.pasha@yandex.ru>
2023-08-31 13:53:53 +0300

    Fix pad_divisor for SwinIA and test it

--- tests/test_backbone.py
+++ tests/test_backbone.py
@@ -105,3 +105,23 @@ def test_factor_unet(downsampling: Tuple[str, ...], shape: Tuple[int, ...], fact
     x = backbone(x)
     x = head(x)
     assert x.shape == (2, 1) + shape
+
+
+@pytest.mark.parametrize('window_size,dilations,shuffles,pad_divisor',
+                         [(8, [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], 8),
+                          (8, [1, 2, 4, 2, 1], [1, 1, 1, 1, 1], 32),
+                          (8, [1, 1, 1, 1, 1], [1, 2, 4, 2, 1], 32),
+                          (8, [1, 2, 4, 2, 1], [1, 2, 4, 2, 1], 128),
+                          (8, [1, 4, 1, 1, 1], [1, 1, 1, 4, 1], 32),
+                          ])
+def test_swinia_pad_divisor(window_size: int, dilations: List[int], shuffles: List[int], pad_divisor: int):
+    os.chdir(Path(__file__).parent.parent)  # necessary to resolve interpolations as ${hydra.runtime.cwd}
+    with initialize(version_base=None, config_path="../config/backbone"):
+        cfg = compose(config_name='swinia', return_hydra_config=False,
+                      overrides=['+dataset.n_channels=1', '+dataset.n_dim=2', '+dataset_train.input_size=64',
+                                 '+training.steps=5000', f'backbone.window_size={window_size}',
+                                 f'backbone.dilations={dilations}', f'backbone.shuffles={shuffles}'])
+        OmegaConf.resolve(cfg)  # resolves interpolations as ${hydra.runtime.cwd}
+        print('\n', OmegaConf.to_yaml(cfg))
+
+    assert cfg.backbone.pad_divisor == pad_divisor
