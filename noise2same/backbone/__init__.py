928be3543731d8cdd49e11a7dac43a6924ae1dd0
pchizhov <chizhov.pasha@yandex.ru>
2024-11-08 18:13:19 +0100

    Add SwinIA to __init__.py

--- noise2same/backbone/__init__.py
+++ noise2same/backbone/__init__.py
@@ -1,6 +1,7 @@
 from .swinir import SwinIR
 from .unet import UNet, RegressionHead
+from .swinia import SwinIA
 
 from . import decoder, encoder
 
-__all__ = ["SwinIR", "UNet", "RegressionHead", "decoder", "encoder"]
+__all__ = ["SwinIR", "UNet", "RegressionHead", "decoder", "encoder", 'SwinIA']
